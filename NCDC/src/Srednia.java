5829c312dff02744c64cad7d3769ab79bdcd69b8
Dawid Stasiak <44843766+Bolvis@users.noreply.github.com>
2021-01-12 01:51:27 +0100

    Code reveiw of srednia.java

--- NCDC/src/Srednia.java
+++ NCDC/src/Srednia.java
@@ -3,22 +3,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Srednia {
+
     private static float Wyliczone(String[] list)
     {
-        List<Float> oceny=new ArrayList<>();
-        float dodane=0;
-        for(String item:list)
+        List<Float> oceny = new ArrayList<>();
+        float dodane = 0;
+        int num_of_bad = 0;
+
+        for(String item: list)
         {
-           try{ if(Float.valueOf(item)<=6&&Float.valueOf(item)>=1){oceny.add(Float.valueOf(item));}
-           else System.out.println("Bledna ocena nr:"+ Arrays.asList(list).indexOf(item));}
-           catch(Exception ex){System.out.println("Bledna ocena nr:"+ Arrays.asList(list).indexOf(item));}
+           try{ 
+                if (Float.valueOf(item) <= 6 && Float.valueOf(item) >=1 ){
+                   oceny.add(Float.valueOf(item));
+                }
+                else {
+                    System.out.println("Bledna ocena nr:" + (Arrays.asList(list).indexOf(item) + 1));
+                    num_of_bad++;
+                }
+            }
+            catch(Exception ex) {
+                System.out.println("Bledna ocena nr:" + (Arrays.asList(list).indexOf(item) + 1));
+                num_of_bad++;
+            }
         }
-        for(Float item:oceny)
-        {
+
+        for(Float item:oceny){
             dodane+=item;
         }
-        return dodane/list.length;
+
+        return dodane/(list.length - num_of_bad);
     }
+
     public static void main(String[] args)
     {
         System.out.print(Wyliczone(args));
